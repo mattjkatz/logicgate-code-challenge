c1737024e835ff2bcc0d0c589df0e02beef5ae55
mattjkatz <matthewkatz7@gmail.com>
2022-03-10 14:57:13 -0600

    some final tweaks

--- barn.rb
+++ barn.rb
@@ -1,7 +1,10 @@
 def barn_sort(unsorted_data)
+  # first sort all of the animals by their "barn" color
   animals_sorted_by_color = {}
   unsorted_data.each do |animal|
+    # check to ensure that the item's keys fit into the barn
     if (animal.key?("animal") && animal.key?("barn"))
+      # check if the aniaml's colored barn exists yet, if not create it, and then add the animal to it's corresponding barn
       if !animals_sorted_by_color.key?("#{animal["barn"]}")
         animals_sorted_by_color[animal["barn"]] = []
       end
@@ -14,24 +17,24 @@ def barn_sort(unsorted_data)
     # create the appropriate number of barns and add them to the placeholder array placeholder_barn
     placeholder_barn = []
     number_of_barns = (color[1].length / 4.0).ceil
-    puts "#{color.class}"
-    puts "#{color}"
-    puts
     number_of_barns.times do |index|
       placeholder_barn << {"barn": "Barn_#{color[0]}_#{index + 1}", "animals": []}
     end
     index = 0
-    # iterate through each animal (one color at a time), evenly distributing them into the barns in placeholder_barn
+    # iterate through each animal (one color at a time), evenly distributing them into placeholder barns
     color[1].each do |animal|
+      # each animal gets evenly distributed, moving through all of the barns with a corresponding color
       if index  == number_of_barns
         index = 0
       end
       placeholder_barn[index][:animals] << animal
       index += 1
     end
+    # shovel each sorted place holder barn into the final array
     placeholder_barn.each do |barn|
       livestock_sorted << barn
     end
   end
-  return JSON.pretty_generate(livestock_sorted)
+  # return the Ruby output as JSON
+  return JSON.generate(livestock_sorted)
 end
--- sort.rb
+++ sort.rb
@@ -1,14 +1,15 @@
 # call the data from the JSON file into a variable as a Ruby hash
-
 require "json"
 require_relative "barn"
 
 file = File.read("animals.json")
 data = JSON.parse(file)
+# pull out the data that we need into a variable that we can perform a method on
+livestock = data["livestock"]
 
 begin
-  livestock = data["livestock"]
+  # perform our sorting method on our data, "puts" so that we are able to view it in the terminal
   puts barn_sort(livestock)
 rescue
-  pp "ERROR: JSON is not properly formatted."
+  p "ERROR: JSON is not properly formatted."
 end
